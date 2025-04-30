c0ce60c0c20204da9e44c3b223bd7ba3a8ed1180
ABHISHEK GORE <akabhigore1@gmail.com>
2025-04-30 21:39:46 +0530

    Fix input handling for student numbers in scorecard

--- ch4/scoreccard.py
+++ ch4/scoreccard.py
@@ -1,8 +1,13 @@
 stumarks=[]
 
-s1=(input("Student's number"))
+s1=int(input("Student's number"))
 stumarks.append(s1)
-s2=(input("Student's number"))
+s2=int(input("Student's number"))
 stumarks.append(s2)
+s3=int(input("Student's number"))
+stumarks.append(s3)
+s4=int(input("Student's number"))
+stumarks.append(s4)
+
 stumarks.sort()
 print(stumarks)
