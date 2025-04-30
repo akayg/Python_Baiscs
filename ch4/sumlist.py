d5921a22c29cbbc21f0a46c17666056113342801
ABHISHEK GORE <akabhigore1@gmail.com>
2025-05-01 00:25:53 +0530

    Add initial implementation of sumlist example with basic operations

---
+++ ch4/sumlist.py
@@ -0,0 +1,7 @@
+numb = [2,23,32,1,123,32,1]
+print(sum(numb))
+print(numb.count(1))
+print("hii")
+# Define a list of numbers
+# Print the sum of all numbers in the list
+# Print the count of occurrences of the number 1 in the list
