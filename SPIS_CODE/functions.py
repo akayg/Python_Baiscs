0f26a30d6ae9c5a99fb24eb17c09dcb3ef3177b7
abhishek <akabhigore1@gmail.com>
2025-05-30 06:51:32 +0530

    Add initial implementation of calculate_sum function

---
+++ SPIS_CODE/functions.py
@@ -0,0 +1,9 @@
+def calculate_sum(data1, data2):
+    #All the statements in the block of code must have the same level of indentation
+    result_sum=data1+data2
+    return result_sum
+
+a=int(input('Enter num1 '))
+b=int(input('Enter number 2 '))
+result=calculate_sum(a,b)
+print(result)
